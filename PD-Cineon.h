a753ef9804cbef2491eb0b882534df4a84738be4
Martin Laurier <153002937+AVProductions@users.noreply.github.com>
2025-08-24 20:32:43 -0400

    Add files via upload

---
+++ PD-Cineon.h
@@ -0,0 +1,118 @@
+// Copyright 2022-present Contributors to the photographic-dctl project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/mikaelsundell/photographic-dctls
+
+// clang-format on
+
+// cineonColor
+struct CineonColor
+{
+    float stop;
+    float r;
+    float g;
+    float b;
+};
+
+__CONSTANT__ struct CineonColor cineon_colors[] = {
+    {-8, 0.01f, 0.01f, 0.01f},
+    {-7, 0.05f, 0.05f, 0.05f},
+    {-6, 0.10f, 0.10f, 0.10f},
+    {-5, 0.40f, 0.25f, 0.60f},
+    {-4, 0.20f, 0.45f, 0.70f},
+    {-3, 0.40f, 0.60f, 0.95f},
+    {-2, 0.40f, 0.60f, 0.25f},
+    {-1, 0.60f, 0.90f, 0.55f},
+    {0,  0.50f, 0.50f, 0.50f},
+    {1,  1.00f, 0.95f, 0.25f},
+    {2,  0.90f, 0.50f, 0.25f},
+    {3,  0.90f, 0.60f, 0.25f},
+    {4,  0.90f, 0.30f, 0.20f},
+    {5,  0.90f, 0.35f, 0.30f},
+    {6,  0.90f, 0.90f, 0.90f},
+    {7,  0.95f, 0.95f, 0.95f},
+    {8,  0.99f, 0.99f, 0.99f}
+};
+#define cineon_stops 17
+
+// Cineon curve
+struct CineonCurve
+{
+    float density;
+    float gamma;
+    float bitdepth;
+    int offset;
+    int white;
+};
+
+__DEVICE__ struct CineonCurve cineon_curve() {
+    struct CineonCurve cv;
+    cv.density = 2.046; cv.gamma = 0.6; cv.bitdepth = 1023; cv.offset = 95; cv.white = 685;
+    return cv;
+}
+
+__DEVICE__  float CineonCurve_steps(struct CineonCurve cv) {
+    return (cv.density / cv.bitdepth);
+}
+
+__DEVICE__ float3 CineonCurve_invert_cineon(struct CineonCurve cv, float3 rgb, float3 scale, float3 dmin) {
+    float3 invert = make_float3(cv.offset, cv.offset, cv.offset);
+    invert += scale * log10_f3(div_3f(dmin, rgb)) / CineonCurve_steps(cv);
+    invert /= cv.bitdepth;
+    return invert;
+}
+
+__DEVICE__ float3 CineonCurve_reverse_cineon(struct CineonCurve cv, float3 rgb, float3 scale, float3 dmin) {
+    float3 reverse = rgb;
+    reverse *= cv.bitdepth;
+    reverse -= make_float3(cv.offset, cv.offset, cv.offset);
+    float3 scaled = reverse * CineonCurve_steps(cv) / scale;
+    rgb = dmin / pow10_f3(scaled);
+    return rgb;
+}
+
+__DEVICE__ float3 CineonCurve_lin_cineon(struct CineonCurve cv, float3 rgb) {
+    float scale = CineonCurve_steps(cv) / cv.gamma;
+    float gain = 1.0 - pow10_f((cv.offset - cv.white) * scale);
+    float offset = cv.white / cv.bitdepth;
+    float r = offset + log_f(max_f((rgb.x - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
+    float g = offset + log_f(max_f((rgb.y - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
+    float b = offset + log_f(max_f((rgb.z - 1.0f) * gain + 1.0, 1e-7)) / (cv.bitdepth * log_f(10) * scale);
+    rgb = make_float3(r, g, b);
+    return rgb;
+}
+
+__DEVICE__ float3 CineonCurve_cineon_lin(struct CineonCurve cv, float3 rgb) {
+    float scale = CineonCurve_steps(cv) / cv.gamma;
+    rgb = min_f3(rgb, 1.0f) * cv.bitdepth;
+    float black = pow_f(10.0f, cv.offset * scale);
+    float diff = pow_f(10.0f, cv.white * scale) - black;
+    float r = pow_f(10.0f, rgb.x * scale);
+    float g = pow_f(10.0f, rgb.y * scale);
+    float b = pow_f(10.0f, rgb.z * scale);
+    rgb = (make_float3(r, g, b) - black) / diff;
+    return rgb;
+}
+
+// invert negative to cineon
+__DEVICE__ float3 invert_cineon(float3 rgb, float3 scale, float3 dmin) {
+    struct CineonCurve cv = cineon_curve();
+    return CineonCurve_invert_cineon(cv, rgb, scale, dmin);
+}
+
+// reverse negative to cineon
+__DEVICE__ float3 reverse_cineon(float3 rgb, float3 scale, float3 dmin) {
+    struct CineonCurve cv = cineon_curve();
+    return CineonCurve_reverse_cineon(cv, rgb, scale, dmin);
+}
+
+// convert linear to cineon
+__DEVICE__ float3 lin_cineon(float3 rgb) {
+    struct CineonCurve cv = cineon_curve();
+    return CineonCurve_lin_cineon(cv, rgb);
+}
+
+// convert cineon to linear
+__DEVICE__ float3 cineon_lin(float3 rgb) {
+    struct CineonCurve cv = cineon_curve();
+    return CineonCurve_cineon_lin(cv, rgb);
+}
---
+++ PD-Rec709.h
@@ -0,0 +1,86 @@
+// Copyright 2022-present Contributors to the photographic-dctl project.
+// SPDX-License-Identifier: BSD-3-Clause
+// https://github.com/mikaelsundell/photographic-dctls
+
+// clang-format on
+
+// rec709 curve
+struct Rec709Curve
+{
+    float threshold;
+    float slope;
+    float exp;
+    float scale;
+    float offset;
+};
+
+__DEVICE__ struct Rec709Curve rec709_curve() {
+    struct Rec709Curve cv;
+    cv.threshold = 0.018;
+    cv.slope = 4.5;
+    cv.exp = 2.2;
+    cv.scale = 1.099;
+    cv.offset = 0.099;
+    return cv;
+}
+
+__DEVICE__ float Rec709Curve_lin_rec709(struct Rec709Curve cv, float lin) {
+    return (lin < cv.threshold) ? (lin * cv.slope) : (cv.scale * pow_f(lin, 1 / cv.exp) - cv.offset);
+}
+
+__DEVICE__ float Rec709Curve_rec709_lin(struct Rec709Curve cv, float val) {
+    return (val < Rec709Curve_lin_rec709(cv, cv.threshold)) ? (val / cv.slope) : pow_f((val + cv.offset) / cv.scale, cv.exp);
+}
+
+// rec709 colorspace
+struct Rec709Colorspace
+{
+    struct Matrix rec709_matrix;
+    struct Matrix xyz_matrix;
+};
+
+__DEVICE__ struct Rec709Colorspace rec709_colorspace() {
+    struct Rec709Colorspace cs;
+    // colortool --inputcolorspace Rec709 -v
+    // rec709 matrix
+    cs.rec709_matrix.m00 = 3.2406; cs.rec709_matrix.m01 = -1.5372; cs.rec709_matrix.m02 = -0.4986;
+    cs.rec709_matrix.m03 = -0.9689; cs.rec709_matrix.m04 = 1.8758; cs.rec709_matrix.m05 = 0.0415;
+    cs.rec709_matrix.m06 = 0.0557; cs.rec709_matrix.m07 = -0.2040; cs.rec709_matrix.m08 = 1.0570;
+    // xyz matrix
+    cs.xyz_matrix.m00 = 0.4124; cs.xyz_matrix.m01 = 0.3576; cs.xyz_matrix.m02 = 0.1805;
+    cs.xyz_matrix.m03 = 0.2126; cs.xyz_matrix.m04 = 0.7152; cs.xyz_matrix.m05 = 0.0722;
+    cs.xyz_matrix.m06 = 0.0193; cs.xyz_matrix.m07 = 0.1192; cs.xyz_matrix.m08 = 0.9505;
+    return cs;
+}
+
+__DEVICE__ float3 Rec709Colorspace_xyz_rec709(struct Rec709Colorspace cs, float3 xyz) {
+    return mult_matrix(xyz, cs.rec709_matrix);
+}
+__DEVICE__ float3 Rec709Colorspace_rec709_xyz(struct Rec709Colorspace cs, float3 rec709) {
+    return mult_matrix(rec709, cs.xyz_matrix);
+}
+
+// convert linear to rec709 gamma ~2.4
+__DEVICE__ float3 lin_rec709gamma24(float3 rgb) {
+    struct Rec709Curve cv = rec709_curve();
+    return make_float3(Rec709Curve_lin_rec709(cv, rgb.x), Rec709Curve_lin_rec709(cv, rgb.y), Rec709Curve_lin_rec709(cv, rgb.z));
+}
+
+// convert rec709 gamma ~2.4 to linear
+__DEVICE__ float3 rec709gamma24_lin(float3 rgb) {
+    struct Rec709Curve cv = rec709_curve();
+    return make_float3(Rec709Curve_rec709_lin(cv, rgb.x), Rec709Curve_rec709_lin(cv, rgb.y), Rec709Curve_rec709_lin(cv, rgb.z));
+}    
+
+// convert rec709 to xyz
+__DEVICE__ float3 xyz_rec709(float3 rgb) {
+    struct Rec709Colorspace cs = rec709_colorspace();
+    float3 rec709 = Rec709Colorspace_xyz_rec709(cs, rgb);
+    return rec709;
+}
+
+// convert xyz to rec709
+__DEVICE__ float3 rec709_xyz(float3 rgb) {
+    struct Rec709Colorspace cs = rec709_colorspace();
+    return Rec709Colorspace_rec709_xyz(cs, rgb);
+}    
